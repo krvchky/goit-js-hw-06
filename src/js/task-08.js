df2d8df421948bb50ebbbc84b6ee9bd317cba208
krvchky <kravchukyana05@gmail.com>
2022-12-21 22:15:42 +0200

    upgate task 08

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,18 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit",(event) => {
+    event.preventDefault()
+
+
+const {email,password} = event.currentTarget.elements;
+if(email.value === "" || password.value === "") {
+    alert("Заповніть,будь ласка,всі поля");
+}
+
+const objectByForm  = {
+    email:email.value,
+    password:password.value,
+};
+console.log("Обєкт із введенними данними:",objectByForm);
+form.reset();
+});
