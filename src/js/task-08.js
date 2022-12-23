a17c0e33c803c1b1cb4636de249dbe7bff0bc7da
krvchky <kravchukyana05@gmail.com>
2022-12-23 14:39:03 +0200

    update task 8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,18 +1,38 @@
+// const form = document.querySelector(".login-form");
+
+// form.addEventListener("submit",(event) => {
+//     event.preventDefault()
+
+
+// const {email,password} = event.currentTarget.elements;
+// if(email.value === "" || password.value === "") {
+//     alert("Заповніть,будь ласка,всі поля");
+// }
+
+// const objectByForm  = {
+//     email:email.value,
+//     password:password.value,
+// };
+// console.log("Обєкт із введенними данними:",objectByForm);
+// form.reset();
+// });
+
 const form = document.querySelector(".login-form");
 
-form.addEventListener("submit",(event) => {
-    event.preventDefault()
+function onSubmit(evt) {
+    evt.preventDefault();
 
+    if(!form.password.value || !form.email.value) {
+        alert("Заповніть,будь ласка,всі поля")
+    } else {
+        const elms = evt.currentTarget.elements;
+        const email = elms.email.value;
+        const password = elms.password.value;
 
-const {email,password} = event.currentTarget.elements;
-if(email.value === "" || password.value === "") {
-    alert("Заповніть,будь ласка,всі поля");
+        const regDataObject = { email, password}
+        console.log(regDataObject)
+        form.reset()
+    }
 }
 
-const objectByForm  = {
-    email:email.value,
-    password:password.value,
-};
-console.log("Обєкт із введенними данними:",objectByForm);
-form.reset();
-});
+form.addEventListener("submit", onSubmit)
